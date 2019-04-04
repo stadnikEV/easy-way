370feeff8d41fce0c2edff1f82ddc60c7b21677b
stadnikEV <stadnik24fps@gmail.com>
2019-04-04 16:30:36 +0300

    replace ё->e for all fio

--- libs/get-words-name.js
+++ libs/get-words-name.js
@@ -3,6 +3,9 @@ module.exports = ({ string }) => {
   if (typeof string !== 'string') {
     return [];
   }
+  string = string.replace(/ё/g, 'е');
+  string = string.replace(/Ё/g, 'Е');
+
   const word = string.match(/([a-zA-Z0-9а-яА-ЯёЁ]+\s*-\s*[a-zA-Z0-9а-яА-ЯёЁ]+|[a-zA-Z0-9а-яА-ЯёЁ]+)/g);
   if (!word) {
     return [];
